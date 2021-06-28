b26a5c06dffcf2ffbacfade1a3e053ddfb5b7fe3
Thacher Hussain <thacher.hussain@gmail.com>
2021-06-28 11:00:42 -0700

    Delete firebaseConfig

--- src/firebase/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBiHHksnrlfduA8nezqSmkJACtiEMZHEnw',
-  authDomain: 'gear-track-app.firebaseapp.com',
-  projectId: 'gear-track-app',
-  storageBucket: 'gear-track-app.appspot.com',
-  messagingSenderId: '863695521070',
-  appId: '1:863695521070:web:31c52fb9ea1bb17ff226bf',
-}
