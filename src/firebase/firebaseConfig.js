0baf43bf06982c179aeafc8579fdec74c013509d
Thacher Hussain <thacher.hussain@gmail.com>
2021-06-28 11:04:01 -0700

    updated gitignore and removed credentials

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,5 +4,5 @@ export default {
   projectId: 'gear-track-app',
   storageBucket: 'gear-track-app.appspot.com',
   messagingSenderId: '863695521070',
-  appId: '1:863695521070:web:31c52fb9ea1bb17ff226bf',
+  appId: '1:863695521070:web:d6b543e4f85d4f96f226bf',
 }
